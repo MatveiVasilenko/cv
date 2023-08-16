afbf17674f0e86c7bd9585cd90586b0bca46847a
MatVasilenko <mat.vasylenko@symmetre.com>
2023-08-16 23:55:12 +0300

    change server

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,5 @@
 export type TLang = 'en' | 'ua';
 
-export const baseUrl = 'https://api-cv.mion.company/api/';
+export const baseUrl = 'https://api-cv.atmk.com.ua/api/';
 export const DEFAULT_LANG = 'en';
 export const DEFAULT_PROJECT_ID = '1';
